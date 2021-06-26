ff4412b94dc35a6bdaf7c4f74355659e5b430a7e
hyperdefined <contact@hyper.lol>
2021-06-25 21:15:28 -0400

    create empty set

--- src/main/java/lol/hyper/perworldchat/PerWorldChat.java
+++ src/main/java/lol/hyper/perworldchat/PerWorldChat.java
@@ -10,6 +10,7 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -39,7 +40,7 @@ public final class PerWorldChat extends JavaPlugin {
 
         // initialize all the worlds
         for (World world : Bukkit.getWorlds()) {
-            Set<Player> empty = new HashSet<>();
+            Set<Player> empty = Collections.emptySet();
             playerLocations.put(world, empty);
         }
     }
